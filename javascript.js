a7d2716ccfbc741f1b0cc4a2a060429f03e31062
tbg1230 <148095689+tbg1230@users.noreply.github.com>
2023-11-09 13:11:18 -0500

    Completed game. runs in a loop 5 times

--- javascript.js
+++ javascript.js
@@ -1,20 +1,51 @@
 //create a variable for computer imput
 //computer imput should be random
+for (let i = 0; i < 5; i++) {
+
 let randomChoice = Math.floor(Math.random()*3);
 
 function getComputerChoice(randomChoice) {
     if (randomChoice == 0) {
-        return randomChoice = "Rock"
+        return randomChoice = "ROCK"
     } else if (randomChoice == 1) {
-        return randomChoice = "Paper"
+        return randomChoice = "PAPER"
     } else {
-        return randomChoice = "Scissors"
+        return randomChoice = "SCISSOR"
     }
 }
 
-console.log(getComputerChoice(randomChoice));
+let computerChoice = getComputerChoice(randomChoice)
+
+
 //create a variable for user writen imput
 //user imput should accept upper, lower, and mixed cases
+
+let userChoice = prompt("Choose Rock, Paper, or Scissor: ").toUpperCase();
+
+
+
+
+
+if (userChoice === computerChoice) {
+    console.log("Both Chose " + userChoice + ". It's a TIE")
+} else if (userChoice === "ROCK" && computerChoice === "PAPER"){
+    console.log("You Chose " + userChoice + " and Computer Chose " + computerChoice + ". YOU LOSE!")
+} else if (userChoice === "ROCK" && computerChoice === "SCISSOR") {
+    console.log("You Chose " + userChoice + " and Computer Chose " + computerChoice + ". YOU WIN!")
+} else if (userChoice === "PAPER" && computerChoice === "SCISSOR"){
+    console.log("You Chose " + userChoice + " and Computer Chose " + computerChoice + ". YOU LOSE!")
+} else if (userChoice === "PAPER" && computerChoice === "ROCK") {
+    console.log("You Chose " + userChoice + " and Computer Chose " + computerChoice + ". YOU WIN!")
+} else if (userChoice === "SCISSOR" && computerChoice === "ROCK"){
+    console.log("You Chose " + userChoice + " and Computer Chose " + computerChoice + ". YOU LOSE!")
+} else if (userChoice === "SCISSOR" && computerChoice === "PAPER"){
+    console.log("You Chose " + userChoice + " and Computer Chose " + computerChoice + ". YOU WIN!")
+} else {
+    userChoice = prompt("INVALID CHOICE! Please Choose Rock, Paper, or Scissor: ").toUpperCase();
+}
+
+}
+// make sure that spelling is correct. Infinite loop needed until correct
 // create function where if same state 'tie'
 // if player chooses rock and comp picks scissors 'you win'
 // if player chooses rock and com picks paper 'you lose'
@@ -22,4 +53,5 @@ console.log(getComputerChoice(randomChoice));
 // if player chooses paper and com picks scissors 'you lose'
 // if player chooses scissors and comp picks paper 'you win'
 // if player chooses scissors and com picks rock 'you lose'
+
 //loop for 5 times
